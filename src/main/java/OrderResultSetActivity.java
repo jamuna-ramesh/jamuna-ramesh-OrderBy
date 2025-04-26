8d25ef8fa7039dd4ecd13fa691ba7b66fbb90bb5
jamuna-ramesh <jamuna26062002@gmail.com>
2025-04-26 15:08:52 +0530

    Done

--- src/main/java/OrderResultSetActivity.java
+++ src/main/java/OrderResultSetActivity.java
@@ -1,6 +1,6 @@
 
 import Util.ConnectionUtil;
-import Util.FileUtil;
+//import Util.FileUtil;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -45,7 +45,8 @@ public class OrderResultSetActivity {
          * Problem 1: Write a statement below to query the database for all characters. Make sure the results are in
          * ascending order by last name, and first name as a tie-breaker.
          */
-        String sql = FileUtil.parseSQLFile("problem1.sql");
+       //String sql = FileUtil.parseSQLFile("problem1.sql");
+        String sql = "SELECT * FROM character ORDER BY last_name ASC, first_name ASC";
 
 
 
